e1aa612f13df3f9158550e48cf50226e3e6c75c4
Logan Riffell <lkriffell@gmail.com>
2020-10-16 18:17:29 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/seed_csv/customers.csv'), headers: true) do |row|
+  Customer.create( {
+    id: row["id"],
+    first_name: row["first_name"],
+    last_name: row["last_name"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  } )
+end
+
+CSV.foreach(Rails.root.join('lib/seed_csv/merchants.csv'), headers: true) do |row|
+  Merchant.create( {
+    id: row["id"],
+    name: row["name"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  } )
+end
+
+CSV.foreach(Rails.root.join('lib/seed_csv/invoices.csv'), headers: true) do |row|
+  Invoice.create( {
+    id: row["id"],
+    customer_id: row["customer_id"],
+    merchant_id: row["merchant_id"],
+    status: row["status"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  } )
+end
+
+CSV.foreach(Rails.root.join('lib/seed_csv/items.csv'), headers: true) do |row|
+  Item.create( {
+    id: row["id"],
+    name: row["name"],
+    description: row["description"],
+    unit_price: row["unit_price"],
+    merchant_id: row["merchant_id"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  } )
+end
+
+CSV.foreach(Rails.root.join('lib/seed_csv/transactions.csv'), headers: true) do |row|
+  Transaction.create( {
+    id: row["id"],
+    invoice_id: row["invoice_id"],
+    cc_num: row["credit_card_number"],
+    result: row["result"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  } )
+end
+
+CSV.foreach(Rails.root.join('lib/seed_csv/invoice_items.csv'), headers: true) do |row|
+  InvoiceItem.create( {
+    id: row["id"],
+    item_id: row["item_id"],
+    invoice_id: row["invoice_id"],
+    quantity: row["quantity"],
+    unit_price: row["unit_price"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  } )
+end
