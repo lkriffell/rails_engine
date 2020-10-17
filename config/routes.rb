8debfeb300de8f32ee76f1fde175938cb46101c9
Logan Riffell <lkriffell@gmail.com>
2020-10-16 18:17:35 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, except: [:new, :edit] do
+        get '/items', to: 'relations#index'
+      end
+      resources :items, except: [:new, :edit] do
+        get '/merchant', to: 'relations#show'
+      end
+      get ':resource/find', to: 'search#show'
+      get ':resource/find_all', to: 'search#index'
+    end
+  end
 end
