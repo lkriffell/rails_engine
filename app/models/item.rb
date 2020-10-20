3d161598786290c48ec737f32aef8f2b62782855
Logan Riffell <lkriffell@gmail.com>
2020-10-20 14:47:43 -0600

    Update model relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   validates :name, :description, :unit_price, :merchant_id, presence: true
   belongs_to :merchant
+  has_many :invoice_items, dependent: :delete_all
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,5 @@
 class Transaction < ApplicationRecord
+  belongs_to :invoice
+
+  scope :paid, -> { where(result: 'success') }
 end
