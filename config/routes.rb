a090d7fc35b346bfb90f130a78bc589e311e9b5f
Logan Riffell <lkriffell@gmail.com>
2020-10-21 15:21:36 -0600

    Add welcome page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/home', to: 'welcome#index'
+  get '/', to: 'welcome#index'
   namespace :api do
     namespace :v1 do
       get ':resource/find', to: 'search#show'
