307be50dd0d47e0109d34d9d2ffbd963d7251484
Logan Riffell <lkriffell@gmail.com>
2020-10-20 20:14:14 -0600

    Add serializer method in total controller to serialize only revenue

--- app/controllers/api/v1/totals_controller.rb
+++ app/controllers/api/v1/totals_controller.rb
@@ -3,12 +3,23 @@ class Api::V1::TotalsController < ApplicationController
     start_date = request.query_parameters['start']
     stop_date = request.query_parameters['end']
     total_revenue = Merchant.total_revenue(start_date, stop_date)
-    require "pry"; binding.pry
-    render json: MerchantSerializer.new(total_revenue)
+    render json: revenue_serializer(total_revenue)
   end
 
   def show
     merchant = Merchant.find(params["merchant_id"])
     render json: MerchantSerializer.new(merchant)
   end
+
+  private
+
+    def revenue_serializer(revenue)
+      {
+        "data": {
+          "attributes": {
+            "revenue": revenue
+          }
+        }
+      }
+    end
 end
